156e126ba2b636b618c00878bc8aaede1266c978
Chokss54 <joelyang54@gmail.com>
2024-08-13 18:00:18 +1000

    added Malay translation option

---
+++ src/localisation/locale/bm.js
@@ -0,0 +1,31 @@
+/**
+ * This file stores all Malay strings that is used statically in the app
+ */
+
+const bm = {
+  //header
+  "TMAPP.title": "Pengurus Tugas",
+  "TMAPP.description": "Ini adalah aplikasi dummy kecil untuk mengurus tugas.",
+
+  //popup
+  "TMAPP.popup.add.header": "Tambah Tugas Baru",
+  "TMAPP.popup.edit.header": "Edit Tugas",
+
+  //buttons
+  "TMAPP.button.newTask": "Tambah Tugas Baru",
+  "TMAPP.button.save": "Simpan Perubahan",
+  "TMAPP.button.close": "Tutup",
+
+  //form
+  "TMAPP.input.title": "Tajuk:",
+  "TMAPP.input.description": "Deskripsi:",
+  "TMAPP.input.dueDate": "Tarikh Akhir:",
+
+  //task
+  "TMAPP.task.dueDate": "Tarikh Akhir: ",
+
+  //weather
+  "TMAPP.weather.celsius": "°C",
+}
+
+export default bm;
